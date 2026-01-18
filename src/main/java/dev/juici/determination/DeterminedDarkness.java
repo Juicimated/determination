2f599157bf2812f1906e7793938d3d70c44f7122
Jaylan Smith <imjuicimated@gmail.com>
2026-01-18 02:05:59 -0500

    Initial commit

---
+++ src/main/java/dev/juici/determination/DeterminedDarkness.java
@@ -0,0 +1,22 @@
+package dev.juici.determination;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import dev.juici.determination.command.TestCommand;
+
+public class DeterminedDarkness extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public DeterminedDarkness(JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from %s version %s", this.getName(),
+                this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        this.getCommandRegistry().registerCommand(new TestCommand(this.getName(),
+                this.getManifest().getVersion().toString()));
+    }
+}
